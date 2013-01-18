5d08c27a19387aaa7d873f3b5fc9424e62fab419
Tom Ridge <tom.ridge@bigpond.com>
2013-01-18 16:54:46 +1000

    some checks and balances

--- app/app.rb
+++ app/app.rb
@@ -13,10 +13,10 @@ module Application
 		end
 
 		post '/upload/?' do
-			upload(params[:file][:filename], params[:file][:tempfile])
+			upload(params[:file][:filename], params[:file][:tempfile]) if params[:file] && params[:file][:filename]
 			asset = Asset.new()
-			asset.path = params[:file][:filename]
-			asset.save
+			asset.path = params[:file][:filename] if params[:file] && params[:file][:filename]
+			halt 400, form_errors.from_model(asset.errors).to_json unless asset.valid? and asset.save
 		end
 
 		delete '/assets/:asset_id/?' do
